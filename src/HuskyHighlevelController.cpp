348225ea8bac6d40d83935d6172d91c703bcf9aa
Diego Calderon <diegocaldeleon1@gmail.com>
2017-04-25 17:11:30 +0200

    Remove unnecessary code and add comments

--- src/HuskyHighlevelController.cpp
+++ src/HuskyHighlevelController.cpp
@@ -2,14 +2,18 @@
 #include <ros/ros.h>
 
 namespace husky_highlevel_controller {
+
   HuskyHighlevelController::HuskyHighlevelController(ros::NodeHandle& nodeHandle)
       : nodeHandle_(nodeHandle)
   {
+    // check if the parameters exists, and if not exists send error and shutdown
     if (!(nodeHandle.getParam("scan_topic_name", topic_name_)
             & nodeHandle.getParam("scan_topic_queue_size", topic_size_queue_))) {
       ROS_ERROR("Could not read parameters.");
       ros::requestShutdown();
     }
+
+    // create a subscriber to the topic_name
     subscriber_ = nodeHandle_.subscribe(topic_name_, 1,
                                         &HuskyHighlevelController::scanCallback, this);
     ROS_INFO("Successfully launched node.");
@@ -19,8 +23,8 @@ namespace husky_highlevel_controller {
   {
   }
 
-
   void HuskyHighlevelController::scanCallback(const sensor_msgs::LaserScan& msg) {
+    // search for the min value in the vector of msg.ranges
     std::vector<float> ranges = msg.ranges;
     float small = ranges[0];
     for (float v:ranges) {
@@ -28,6 +32,8 @@ namespace husky_highlevel_controller {
         small = v;
       }
     }
+
+    // display the ming value
     ROS_INFO_STREAM("Min range: " + std::to_string(small));
 
   }
--- src/husky_highlevel_controller_node.cpp
+++ src/husky_highlevel_controller_node.cpp
@@ -3,15 +3,9 @@
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "husky_highlevel_controller");
-  ros::NodeHandle nodeHandle;
 
-//
-//  std::string topic_name;
-//  int queue_size;
-//  nodeHandle.getParam("scan_topic_name", topic_name);
-//  nodeHandle.getParam("scan_topic_queue_size", queue_size);
-//  ros::Subscriber subscriber = nodeHandle.subscribe(topic_name, queue_size, &scanCallback);
-  //ros::Subscriber subscriber = nodeHandle.subscribe("/scan", 10, scanCallback);
+  // create a (Default) public nodeHandle
+  ros::NodeHandle nodeHandle;
 
   husky_highlevel_controller::HuskyHighlevelController huskyHighlevelController(nodeHandle);
 
